4186d30c4d03c3066c3dd8e4b99f9fc851c199e8
adomurad <adomurad@localhost.localdomain>
2015-10-17 01:03:01 -0400

    Reached goodness

--- catedit.js
+++ catedit.js
@@ -1,10 +1,17 @@
-var temp = require('temp');
 var fs = require('fs');
+var temp = require('temp');
+var path = require('path');
+var clc = require('cli-color');
 var _a = require('child_process'), exec = _a.exec, spawn = _a.spawn;
 var args = process.argv.map(function (s) { return s; });
 args.shift();
 args.shift(); // Pop front two arguments
-temp.track();
+temp.mkdir('catedit', function (err, dirPath) {
+    if (err)
+        throw err;
+    combineInputIntoTemporaryFile(dirPath);
+});
+// Helper functions from here out
 function computeNewContents(fileContent) {
     var currentFileName = null;
     var buffer = [];
@@ -14,8 +21,8 @@ function computeNewContents(fileContent) {
         var endMatch = line.match(/^\s*@@\s*END\s*@@\s*$/);
         if (endMatch) {
             newContents[currentFileName] = buffer.join('\n') + '\n';
-            currentFileName = null;
             buffer = [];
+            currentFileName = null;
             continue;
         }
         var match = line.match(/^\s*@@\s*(.+)\s*@@\s*$/);
@@ -28,6 +35,56 @@ function computeNewContents(fileContent) {
     }
     return newContents;
 }
+function combineInputIntoTemporaryFile(tempDirPath) {
+    var contentList = args.map(function (fname) { return ("@@" + fname + "@@\n" + readIfExists(fname) + "@@END@@\n"); });
+    // Provides some hint to the files inside:
+    var cateditFileName = "catedit_" + args.join('_').replace(/[^a-zA-Z0-9@]/g, '@');
+    if (cateditFileName.length > 25) {
+        cateditFileName = cateditFileName.substring(0, 22) + '...';
+    }
+    var filePath = path.join(tempDirPath, cateditFileName);
+    fs.writeFileSync(filePath, contentList.join('\n'));
+    launchVim(filePath);
+}
+function launchVim(filePath) {
+    var vimInstance = spawn('vim', [filePath, '-c', 'silent set filetype=typescript'], { stdio: 'inherit' });
+    function afterVimCloses() {
+        updateFilesAccordingToChanges(filePath);
+    }
+    vimInstance.on('exit', afterVimCloses);
+}
+function updateFilesAccordingToChanges(filePath) {
+    var fileContent = fs.readFileSync(filePath, 'utf8');
+    var newContent = computeNewContents(fileContent);
+    var removed = args; //copy
+    for (var _i = 0, _a = Object.keys(newContent); _i < _a.length; _i++) {
+        var key = _a[_i];
+        var existed = fs.existsSync(key);
+        // We delete files no longer mentioned
+        fs.writeFileSync(key, newContent[key]);
+        removed = removed.filter(function (fname) { return fname !== key; });
+        if (!existed) {
+            console.log(clc.green("Wrote new file '" + key + "'."));
+        }
+        else {
+            console.log(clc.yellow("Updated '" + key + "'."));
+        }
+    }
+    deleteRemovedEntries(removed);
+}
+function deleteRemovedEntries(removed) {
+    // Remove the removed:
+    for (var _i = 0; _i < removed.length; _i++) {
+        var fileName = removed[_i];
+        try {
+            fs.unlinkSync(fileName);
+            console.log(clc.red("Deleted '" + fileName + "'."));
+        }
+        catch (err) {
+            console.log(clc.blackBright("Nothing to write with '" + fileName + "'."));
+        }
+    }
+}
 function readIfExists(fname) {
     try {
         return fs.readFileSync(fname, 'utf8');
@@ -36,34 +93,3 @@ function readIfExists(fname) {
         return '';
     }
 }
-temp.open('catedit', function (err, info) {
-    if (!err) {
-        var contentList = args.map(function (fname) { return ("@@" + fname + "@@\n" + readIfExists(fname) + "@@END@@\n\n"); });
-        for (var _i = 0; _i < contentList.length; _i++) {
-            var content = contentList[_i];
-            fs.writeSync(info.fd, content);
-        }
-        fs.close(info.fd, function () {
-            var vimInstance = spawn('vim', [info.path, '-c', 'silent set filetype=typescript'], { stdio: 'inherit' });
-            vimInstance.on('exit', function () {
-                var fileContent = fs.readFileSync(info.path, 'utf8');
-                var newContent = computeNewContents(fileContent);
-                var missing = args; //copy
-                for (var _i = 0, _a = Object.keys(newContent); _i < _a.length; _i++) {
-                    var key = _a[_i];
-                    // TODO delete files no longer mentioned?
-                    fs.writeFileSync(key, newContent[key]);
-                    missing = missing.filter(function (fname) { return fname !== key; });
-                }
-                // Remove the missing:
-                for (var _b = 0, _c = missing; _b < _c.length; _b++) {
-                    var key_1 = _c[_b];
-                    try {
-                        fs.unlinkSync(key_1);
-                    }
-                    catch (err) {}
-                }
-            });
-        });
-    }
-});
--- catedit.ts
+++ catedit.ts
@@ -1,31 +1,86 @@
 declare var require, process;
 
-var temp = require('temp');
 var fs = require('fs');
+var temp = require('temp');
+var path = require('path');
+var clc = require('cli-color');
 var {exec,spawn}   = require('child_process');
 
 var args = process.argv.map(s => s); args.shift(); args.shift(); // Pop front two arguments
-temp.track();
+
+temp.mkdir('catedit', (err,dirPath) => {
+    if (err) throw err;
+    combineInputIntoTemporaryFile(dirPath);
+})
+
+// Helper functions from here out
 
 function computeNewContents(fileContent) {
     let currentFileName = null;
     let buffer = [];
     let newContents = {};
     for (let line of fileContent.split('\n')) {
-        let endMatch = line.match(/^\s*\/\/\s*@END@\s*$/);
+        let endMatch = line.match(/^\s*@@\s*END\s*@@\s*$/);
         if (endMatch) {
             newContents[currentFileName] = buffer.join('\n') + '\n';
             buffer = [];
+            currentFileName = null;
             continue;
         }
-        let match = line.match(/^\s*\/\/\s*@@\s*(.+)\s*$/);
+        let match = line.match(/^\s*@@\s*(.+)\s*@@\s*$/);
         if (match) { 
             currentFileName = match[1];
-        } else {
+        } else if (currentFileName !== null) {
             buffer.push(line);
         }
     }
     return newContents;
+}
+
+function combineInputIntoTemporaryFile(tempDirPath) {
+    let contentList:string[] = args.map(fname => `@@${fname}@@\n${readIfExists(fname)}@@END@@\n`);
+    // Provides some hint to the files inside:
+    let cateditFileName = "catedit_" + args.join('_').replace(/[^a-zA-Z0-9@]/g, '@');
+    if (cateditFileName.length > 25) {
+        cateditFileName = cateditFileName.substring(0, 22) + '...';
+    }
+    let filePath = path.join(tempDirPath, cateditFileName);
+    fs.writeFileSync(filePath, contentList.join('\n'));
+    launchVim(filePath)
+}
+
+function launchVim(filePath) {
+    let vimInstance = spawn('vim', [filePath, '-c', 'silent set filetype=typescript'], { stdio: 'inherit' });
+    function afterVimCloses() {
+        updateFilesAccordingToChanges(filePath);
+    }
+    vimInstance.on('exit', afterVimCloses);
+}
+
+function updateFilesAccordingToChanges(filePath) {
+    let fileContent = fs.readFileSync(filePath, 'utf8');
+    let newContent = computeNewContents(fileContent);
+    let removed = args; //copy
+    for (var key of Object.keys(newContent)) {
+        let existed = fs.existsSync(key);
+        // We delete files no longer mentioned
+        fs.writeFileSync(key, newContent[key]);
+        removed = removed.filter(fname => fname !== key);
+        if (!existed) {
+            console.log(clc.green(`Wrote new file '${key}'.`));
+        } else {
+            console.log(clc.yellow(`Updated '${key}'.`));
+        }
+    }
+    deleteRemovedEntries(removed);
+}
+
+function deleteRemovedEntries(removed) {
+    // Remove the removed:
+    for (let fileName of removed) {
+        try { fs.unlinkSync(fileName); console.log(clc.red(`Deleted '${fileName}'.`)); } 
+        catch(err){ console.log(clc.blackBright(`Nothing to write with '${fileName}'.`));}
+    }
 }
 
 function readIfExists(fname) {
@@ -36,29 +91,4 @@ function readIfExists(fname) {
     }
 }
 
-temp.open('catedit', (err,info) => {
-    if (!err) {
-        var contentList:string[] = args.map(fname => `//@@${fname}\n${readIfExists(fname)}//@END@\n`);
-        for (let content of contentList) {
-            fs.writeSync(info.fd, content);
-        }
-        fs.close(info.fd, () => {
-            let vimInstance = spawn('vim', [info.path, '+'], {stdio: 'inherit'});
-            vimInstance.on('exit', () => {
-                let fileContent = fs.readFileSync(info.path, 'utf8');
-                let newContent = computeNewContents(fileContent);
-                let missing = args; //copy
-                for (var key of Object.keys(newContent)) {
-                    // TODO delete files no longer mentioned?
-                    fs.writeFileSync(key, newContent[key]);
-                    missing = missing.filter(fname => fname !== key);
-                }
-                // Remove the missing:
-                for (let key of Object.keys(missing)) {
-                    try { fs.unlinkSync(key); } catch(err){}
-                }
-            });
-        });
-        
-    }
-});
+
